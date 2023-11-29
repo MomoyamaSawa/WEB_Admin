2560487313271ecc5c9eacec54c3570616e9c012
うさぎ <momoyama.sawa@gmail.com>
2023-11-29 11:03:02 +0800

    chore: 项目配置&集成

---
+++ src/api/user/index.ts
@@ -0,0 +1,2 @@
+// 统一管理用户相关的接口
+import request from '@/utils/request'
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import gloablComponent from './components/index'
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
+import '@/styles/index.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.use(gloablComponent)
+app.mount('#app')
