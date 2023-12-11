5e84928ff58134121822513be7eebf684a031fe2
うさぎ <momoyama.sawa@gmail.com>
2023-12-11 16:15:39 +0800

    feat: 展示已有数据

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -14,15 +14,21 @@
         <el-button type="primary" size="default" @click="">添加用户</el-button>
         <el-button type="primary" size="default" @click="">批量删除</el-button>
         <!-- 表格展示用户信息 -->
-        <el-table style="margin: 10px 0px" border>
+        <el-table style="margin: 10px 0px" border :data="userArr">
             <el-table-column type="selection" align="center"></el-table-column>
-            <el-table-column label="#" align="center"></el-table-column>
-            <el-table-column label="用户名" align="center"></el-table-column>
-            <el-table-column label="用户名称" align="center"></el-table-column>
-            <el-table-column label="角色" align="center"></el-table-column>
-            <el-table-column label="创建时间" align="center"></el-table-column>
-            <el-table-column label="更新时间" align="center"></el-table-column>
-            <el-table-column label="操作" width="260px" align="center"></el-table-column>
+            <el-table-column type="index" label="#" align="center"></el-table-column>
+            <el-table-column label="用户名" align="center" prop="username" show-overflow-tooltip></el-table-column>
+            <el-table-column label="用户名称" align="center" prop="name" show-overflow-tooltip></el-table-column>
+            <el-table-column label="角色" align="center" prop="roleName" show-overflow-tooltip></el-table-column>
+            <el-table-column label="创建时间" align="center" prop="createTime" show-overflow-tooltip></el-table-column>
+            <el-table-column label="更新时间" align="center" prop="updateTime" show-overflow-tooltip></el-table-column>
+            <el-table-column label="操作" width="300px" align="center">
+                <template #="{ row, $index }">
+                    <el-button type="warning" size="small" icon="User" @click="">分配角色</el-button>
+                    <el-button type="primary" size="small" icon="Edit" @click="">编辑</el-button>
+                    <el-button type="danger" size="small" icon="Delete" @click="">删除</el-button>
+                </template>
+            </el-table-column>
         </el-table>
         <!-- 分页器 -->
         <el-pagination
@@ -30,18 +36,35 @@
             v-model:page-size="pageSize"
             :page-sizes="[5, 7, 9, 11]"
             :background="true"
-            layout="prev, pager, next, jumper,->,total, sizes"
-            :total="400"
-            @size-change=""
-            @current-change=""
+            layout="prev, pager, next, jumper,->,sizes,total"
+            :total="total"
+            @size-change="handler"
+            @current-change="getHasuser"
         />
     </el-card>
 </template>
 
 <script setup lang="ts" name="User">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { reqUserInfo } from '@/api/acl/user'
+import type { UserResponseData, Records } from '@/api/acl/user/type'
+// 初始化
 let pageNo = ref<number>(1)
 let pageSize = ref<number>(5)
+let total = ref<number>(0)
+let userArr = ref<Records>([])
+onMounted(() => {
+    getHasuser()
+})
+const getHasuser = (pageNo = 1) => {
+    reqUserInfo(pageNo, pageSize.value).then((res: UserResponseData) => {
+        userArr.value = res.data.records
+        total.value = res.data.total
+    })
+}
+const handler = () => {
+    getHasuser()
+}
 </script>
 
 <style scoped lang="scss">
