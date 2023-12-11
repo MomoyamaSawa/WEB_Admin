19166fd38264b8dccee2e660914a071231bba054
うさぎ <momoyama.sawa@gmail.com>
2023-12-11 17:11:52 +0800

    feat: 添加新账号业务

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -11,8 +11,8 @@
         </el-form>
     </el-card>
     <el-card style="margin: 10px 0px">
-        <el-button type="primary" size="default" @click="">添加用户</el-button>
-        <el-button type="primary" size="default" @click="">批量删除</el-button>
+        <el-button type="primary" size="default" @click="addUser">添加用户</el-button>
+        <el-button type="danger" size="default" @click="">批量删除</el-button>
         <!-- 表格展示用户信息 -->
         <el-table style="margin: 10px 0px" border :data="userArr">
             <el-table-column type="selection" align="center"></el-table-column>
@@ -25,7 +25,7 @@
             <el-table-column label="操作" width="300px" align="center">
                 <template #="{ row, $index }">
                     <el-button type="warning" size="small" icon="User" @click="">分配角色</el-button>
-                    <el-button type="primary" size="small" icon="Edit" @click="">编辑</el-button>
+                    <el-button type="primary" size="small" icon="Edit" @click="editUser(row)">编辑</el-button>
                     <el-button type="danger" size="small" icon="Delete" @click="">删除</el-button>
                 </template>
             </el-table-column>
@@ -42,12 +42,38 @@
             @current-change="getHasuser"
         />
     </el-card>
+    <!-- 抽屉结构 -->
+    <el-drawer v-model="drawer">
+        <template #header>
+            <h4>添加用户</h4>
+        </template>
+        <template #default>
+            <el-form ref="form" :rules="rules" :model="userParams">
+                <el-form-item label="用户姓名" prop="username">
+                    <el-input placeholder="请输入用户名" v-model="userParams.username"></el-input>
+                </el-form-item>
+                <el-form-item label="用户昵称" prop="name">
+                    <el-input placeholder="请输入用户昵称" v-model="userParams.name"></el-input>
+                </el-form-item>
+                <el-form-item label="用户密码" prop="password">
+                    <el-input placeholder="请输入密码" v-model="userParams.password"></el-input>
+                </el-form-item>
+            </el-form>
+        </template>
+        <template #footer>
+            <div style="flex: auto">
+                <el-button @click="cancelClick">取消</el-button>
+                <el-button type="primary" @click="confirmClick">确定</el-button>
+            </div>
+        </template>
+    </el-drawer>
 </template>
 
 <script setup lang="ts" name="User">
-import { ref, onMounted } from 'vue'
-import { reqUserInfo } from '@/api/acl/user'
-import type { UserResponseData, Records } from '@/api/acl/user/type'
+import { ref, onMounted, reactive, nextTick } from 'vue'
+import { reqUserInfo, reqAddOrUpdateUser } from '@/api/acl/user'
+import type { UserResponseData, Records, User } from '@/api/acl/user/type'
+import { ElMessage } from 'element-plus'
 // 初始化
 let pageNo = ref<number>(1)
 let pageSize = ref<number>(5)
@@ -64,6 +90,76 @@ const getHasuser = (pageNo = 1) => {
 }
 const handler = () => {
     getHasuser()
+}
+
+// 抽屉
+let drawer = ref<boolean>(false)
+const validatorUsername = (rule: any, value: string, callback: any) => {
+    if (value.trim().length <= 5) {
+        return callback(new Error('用户名称至少5位'))
+    }
+    callback()
+}
+const validatorName = (rule: any, value: string, callback: any) => {
+    if (value.trim().length <= 5) {
+        return callback(new Error('用户昵称至少5位'))
+    }
+    callback()
+}
+const validatorPassword = (rule: any, value: string, callback: any) => {
+    if (value.trim().length <= 6) {
+        return callback(new Error('用户密码至少6位'))
+    }
+    callback()
+}
+const rules = {
+    username: [{ required: true, validator: validatorUsername, trigger: 'blur' }],
+    name: [{ required: true, validator: validatorName, trigger: 'blur' }],
+    password: [{ required: true, validator: validatorPassword, trigger: 'blur' }],
+}
+let userParams = reactive<User>({
+    username: '',
+    name: '',
+    password: '',
+})
+const addUser = () => {
+    // 数据清空
+    Object.assign(userParams, {
+        username: '',
+        name: '',
+        password: '',
+    })
+    drawer.value = true
+    // 清除警告捏
+    nextTick(() => {
+        form.value.clearValidate()
+    })
+}
+const editUser = (row: User) => {
+    drawer.value = true
+}
+let form = ref()
+const confirmClick = () => {
+    // 先进行校验
+    form.value
+        .validate()
+        .then((res: any) => {
+            reqAddOrUpdateUser(userParams)
+                .then((res: any) => {
+                    drawer.value = false
+                    ElMessage.success('添加成功')
+                    getHasuser()
+                })
+                .catch((err: any) => {
+                    ElMessage.error('添加失败')
+                })
+        })
+        .catch((err: any) => {
+            return
+        })
+}
+const cancelClick = () => {
+    drawer.value = false
 }
 </script>
 
