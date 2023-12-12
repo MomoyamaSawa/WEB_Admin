2410a220171cf13f8d972741361f36d7cd463d87
うさぎ <momoyama.sawa@gmail.com>
2023-12-12 16:41:46 +0800

    feat: 首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,55 @@
 <template>
-    <div>
-        <h1>展示页</h1>
+    <el-card>
+        <div class="box">
+            <img :src="userStore.avatar" alt="" class="avatar" />
+            <div class="bottom">
+                <h3 class="title">{{ getTime() + '好鸭，' + userStore.username }}</h3>
+                <p class="sub">欢迎来到后台管理系统</p>
+            </div>
+        </div>
+    </el-card>
+    <div class="bottoms">
+        <svg-icon name="welcome" width="600px" height="300px"></svg-icon>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts" name="Home">
+// 引入用户相关仓库
+import userUserStore from '@/store/modules/user'
+import { getTime } from '@/utils/time'
+let userStore = userUserStore()
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+    display: flex;
+
+    .avatar {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+
+    .bottom {
+        margin-top: 10px;
+        margin-left: 20px;
+
+        .title {
+            font-size: 30px;
+            font-weight: 900;
+            margin-bottom: 30px;
+        }
+
+        .sub {
+            font-style: italic;
+            color: dimgray;
+        }
+    }
+}
+
+.bottoms {
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+}
+</style>
