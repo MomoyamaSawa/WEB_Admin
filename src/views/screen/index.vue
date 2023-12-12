e76703ebf92b7fb979e644de9ae6222f04f91f73
うさぎ <momoyama.sawa@gmail.com>
2023-12-12 19:45:01 +0800

    feat: 资源&解决适配问题

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,52 @@
 <template>
-    <div>
-        <h1>数据大屏</h1>
+    <div class="container">
+        <!-- 数据大屏内容去 -->
+        <div class="screen" ref="screen">
+            <div class="top">顶部</div>
+            <div class="bottom">
+                <div class="left">左侧</div>
+                <div class="center">中间</div>
+                <div class="right">右侧</div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts" name="Screen">
+import { ref, onMounted } from 'vue'
+/**
+ * 进行适配
+ */
+const screen = ref()
+onMounted(() => {
+    screen.value.style.transform = `translate(-50%, -50%) scale(${getScale()})`
+})
+// 定义缩放比例
+function getScale(w = 1920, h = 1080) {
+    const ww = window.innerWidth / w
+    const wh = window.innerHeight / h
+    return ww > wh ? wh : ww
+}
+// 监听视口变化
+window.onresize = () => {
+    screen.value.style.transform = `translate(-50%, -50%) scale(${getScale()})`
+}
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+    width: 100vw;
+    height: 100vh;
+    background: url('./images/bg.png') no-repeat;
+    background-size: cover;
+
+    .screen {
+        position: fixed;
+        width: 1920px;
+        height: 1080px;
+        background: red;
+        left: 50%;
+        top: 50%;
+    }
+}
+</style>
